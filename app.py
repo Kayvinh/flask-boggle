9a29844c01dc5a9c1a3fefb3718e2cdb5f6f00f6
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-10 16:47:19 -0800

    step 4 complete; code review passed

--- app.py
+++ app.py
@@ -25,14 +25,38 @@ def new_game():
     game_id = str(uuid4())
     game = BoggleGame()
     games[game_id] = game
-    info = {"gameId": game_id, "board": game.board}
 
-
-    return jsonify(info)
+    return jsonify({"game_id": game_id, "board": game.board})
 
 @app.post("/api/score-word")
 def score_word():
+    """ Checks if the submitted word is valid for a board given a game_id
+        TODO: Clarify in first line that will return JSON and not Python objects
+
+        Input: {
+            "game_id": <str: GAME_ID>,
+            "word" : <str: WORD>
+        }
+
+        Outputs:
+            If not in word list:
+                { result: 'not-word' }
+
+            If not on game board:
+                { result: 'not-on-board' }
+
+            If valid word:
+                { result: 'ok'}
+
+    """
+
     word = request.json["word"]
-    game_id = request.json["gameId"]
-    if games[game_id].word_list.check_word(word):
-        #return jsonify("result": "ok")
+    game_id = request.json["game_id"]
+    game = games[game_id]
+
+    if not game.is_word_in_word_list(word):
+        return jsonify(result='not-word')
+    elif not game.check_word_on_board(word):
+        return jsonify(result='not-on-board')
+    else:
+        return jsonify(result='ok')
--- test_app.py
+++ test_app.py
@@ -18,6 +18,8 @@ class BoggleAppTestCase(TestCase):
         self.client = app.test_client()
         app.config['TESTING'] = True
 
+
+
     def test_homepage(self):
         """Make sure information is in the session and HTML is displayed"""
 
@@ -32,9 +34,42 @@ class BoggleAppTestCase(TestCase):
         """Test starting a new game."""
 
         with self.client as client:
-            # write a test for this route
+
             response = client.post('/api/new-game')
-            html = response.get_data(as_text=True)
+            json = response.get_json()
 
             self.assertEqual(response.status_code, 200)
-            self.assertTrue(response.is_json, html)
+            self.assertTrue(isinstance(json['game_id'], str))
+            self.assertTrue(isinstance(json['board'], list))
+            self.assertTrue(isinstance(json['board'][0], list))
+
+    def test_score_word(self):
+        """Test scoring a word in the game."""
+        with self.client as client:
+
+            response = client.post('/api/new-game')
+            breakpoint()
+            game_id = response.json['game_id']
+            game = games[game_id]
+
+            # testing 'not-on-board'
+            # TODO: Reset the board to ensure the test word is not present
+            response = client.post('/api/score-word',
+                                   json={'game_id': game_id,
+                                         'word': 'MEAL'})
+            self.assertEqual({'result': 'not-on-board'}, response.json)
+
+            # testing 'not-word'
+            response = client.post('/api/score-word',
+                                   json={'game_id': game_id,
+                                         'word': 'ASLKdJA'})
+            self.assertEqual({'result': 'not-word'}, response.json)
+
+            # testing 'ok'
+            game.board[0] = ['C', 'A', 'T', 'A', 'B']
+            # TODO: Can do this when setting the board above for the not-on-board test
+
+            response = client.post('/api/score-word',
+                                   json={'game_id': game_id,
+                                         'word': 'CAT'})
+            self.assertEqual({'result': 'ok'}, response.json)
